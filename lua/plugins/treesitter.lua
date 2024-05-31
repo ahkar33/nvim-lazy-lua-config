39f693cb2352ff7f05435b68f0d78941ea191e88
ahkar33 <ahkartoemaw@gmail.com>
2024-05-31 21:38:59 +0630

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+}
