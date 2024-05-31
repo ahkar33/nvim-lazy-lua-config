b5b1d0cfe28b458f1b4ab2ce2e9c70bbb051bab5
ahkar33 <ahkartoemaw@gmail.com>
2024-06-01 01:08:32 +0630

    added harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,132 @@
+return {
+	"theprimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("harpoon"):setup()
+	end,
+
+	keys = function()
+		local harpoon = require("harpoon")
+
+		local conf = require("telescope.config").values
+
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			local make_finder = function()
+				local paths = {}
+
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(paths, item.value)
+				end
+
+				return require("telescope.finders").new_table({
+					results = paths,
+				})
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = false,
+					sorter = conf.generic_sorter({}),
+					layout_strategy = "center",
+					layout_config = {
+						preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
+						width = function(_, max_columns, _)
+							return math.min(max_columns, 80)
+						end,
+
+						height = function(_, _, max_lines)
+							return math.min(max_lines, 15)
+						end,
+					},
+					borderchars = {
+						prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+						results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+						preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					},
+					attach_mappings = function(prompt_buffer_number, map)
+						map("i", "<c-d>", function()
+							local state = require("telescope.actions.state")
+							local selected_entry = state.get_selected_entry()
+							local current_picker = state.get_current_picker(prompt_buffer_number)
+
+							harpoon:list():remove(selected_entry)
+							current_picker:refresh(make_finder())
+						end)
+
+						return true
+					end,
+				})
+				:find()
+		end
+		return {
+			{
+				"<leader>ft",
+				function()
+					toggle_telescope(harpoon:list())
+				end,
+				desc = "Harpoon (Telescope)",
+			},
+			{
+				"<leader>a",
+				function()
+					require("harpoon"):list():add()
+				end,
+				desc = "harpoon file",
+			},
+			{
+				"<leader>h",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "harpoon quick menu",
+			},
+			{
+				"<leader>1",
+				function()
+					require("harpoon"):list():select(1)
+				end,
+				desc = "harpoon to file 1",
+			},
+			{
+				"<leader>2",
+				function()
+					require("harpoon"):list():select(2)
+				end,
+				desc = "harpoon to file 2",
+			},
+			{
+				"<leader>3",
+				function()
+					require("harpoon"):list():select(3)
+				end,
+				desc = "harpoon to file 3",
+			},
+			{
+				"<leader>4",
+				function()
+					require("harpoon"):list():select(4)
+				end,
+				desc = "harpoon to file 4",
+			},
+			{
+				"<leader>5",
+				function()
+					require("harpoon"):list():select(5)
+				end,
+				desc = "harpoon to file 5",
+			},
+		}
+	end,
+}
