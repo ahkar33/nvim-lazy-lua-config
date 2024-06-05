2cbd5f84d9f757073a85f05ac3625afe98743908
ahkar33 <ahkartoemaw@gmail.com>
2024-06-06 00:07:42 +0630

    make hidden file visible in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,16 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, -- this shows hidden files
+          hide_dotfiles = false, -- this ensures dotfiles are not hidden
+          hide_gitignored = false, -- this ensures gitignored files are not hidden
+        },
+      },
+    })
+    
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
     vim.keymap.set("n", "<C-k>", ":Neotree close<CR>")
   end,
