91a0f5a2d9791442d3eb353676e57b09b0e17614
ahkar33 <ahkartoemaw@gmail.com>
2024-06-06 17:23:22 +0630

    filter node module in <gd> key

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,8 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "html", "tailwindcss", "cssls", "jsonls", "gopls"},
+				ensure_installed = { "lua_ls", "tsserver", "html", "tailwindcss", "cssls", "jsonls", "gopls" },
+				automatic_installation = true,
 			})
 		end,
 	},
@@ -18,35 +19,52 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.emmet_language_server.setup({})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        cmd = {"gopls"},
-        filetypes = {"go", "gomod", "gowork", "gotmpl"},
-        root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-      })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+			-- Function to filter out react/index.d.ts
+			local function filterReactDTS(value)
+				return string.match(value.filename, "react/index.d.ts") == nil
+			end
+
+			-- Custom on_list function
+			local function on_list(options)
+				local items = options.items
+				if #items > 1 then
+					items = vim.tbl_filter(filterReactDTS, items)
+				end
+
+				vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
+				vim.api.nvim_command("cfirst") -- or maybe you want 'copen' instead of 'cfirst'
+			end
+
+			-- Common on_attach function
+			local on_attach = function(client, bufnr)
+				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition({ on_list = on_list })
+				end, bufopts)
+			end
+
+			-- Setup LSP servers with common configuration
+			local servers = {
+				emmet_language_server = {},
+				lua_ls = { capabilities = capabilities },
+				html = { capabilities = capabilities },
+				cssls = { capabilities = capabilities },
+				tailwindcss = { capabilities = capabilities },
+				tsserver = { capabilities = capabilities },
+				jsonls = { capabilities = capabilities },
+				gopls = {
+					capabilities = capabilities,
+					cmd = { "gopls" },
+					filetypes = { "go", "gomod", "gowork", "gotmpl" },
+					root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+				},
+			}
+
+			for server, config in pairs(servers) do
+				config.on_attach = on_attach
+				lspconfig[server].setup(config)
+			end
 		end,
 	},
 }
